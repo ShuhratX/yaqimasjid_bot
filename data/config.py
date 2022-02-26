3f766d984ef4bc859bc34bf64abd1c9c8e7fa7e6
ShuhratXayitov <65951559+ShuhratX@users.noreply.github.com>
2022-02-26 21:45:36 +0500

    secret key

--- data/config.py
+++ data/config.py
@@ -9,6 +9,8 @@ BOT_TOKEN = env.str("BOT_TOKEN")  # Bot token
 ADMINS = env.list("ADMINS")  # adminlar ro'yxati
 IP = env.str("ip")  # Xosting ip manzili
 
+SECRET_KEY = env.str("SECRET_KEY")
+
 DB_USER = env.str("DB_USER")
 DB_PASS = env.str("DB_PASS")
 DB_NAME = env.str("DB_NAME")
